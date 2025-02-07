ea8e0b29e1d63b177ed33da866a6e41e3a93fd67
Kaan <kaannkahramannn@gmail.com>
2025-02-08 00:04:23 +0300

    Kod 0.4

---
+++ src/main/java/frc/robot/controller/Driver.java
@@ -0,0 +1,23 @@
+package frc.robot.controller;
+
+import edu.wpi.first.wpilibj.XboxController;
+
+public class Driver {
+    XboxController controller;
+
+    public Driver(int port) {
+        controller = new XboxController(port);
+    }
+
+    public double getSpeed() {
+        return -controller.getLeftY();
+    }
+
+    public double getRotation() {
+        return -controller.getRightX();
+    }
+    
+    public boolean getWantsToScoreCoral() {
+        return controller.getAButton();
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/Drive/DriveTrain.java
@@ -0,0 +1,162 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.Drive;
+
+import org.littletonrobotics.junction.Logger;
+
+//import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.studica.frc.AHRS;
+import com.studica.frc.AHRS.NavXComType;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.numbers.N2;
+import edu.wpi.first.math.system.LinearSystem;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.RobotController;
+//import edu.wpi.first.math.geometry.Pose2d;
+//import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.simulation.Field;
+import frc.robot.simulation.SimulatableSparkMax;
+
+public class DriveTrain extends SubsystemBase {
+
+  SimulatableSparkMax leftFront, leftRear, rightFront, rightRear;
+  AHRS gyro;
+  DifferentialDrive drive;
+ // RelativeEncoder leftEncoder, rightEncoder;
+
+  public static DriveTrain instance;
+  private final Field field = Field.getInstance();
+  private double x = 2.893, y = 7.042;
+  private Rotation2d heading = new Rotation2d();
+
+  private static final double kTrackWidth = Units.inchesToMeters(20.75);
+  private static final double kWheelRadius = Units.inchesToMeters(3.0);
+  private static final double kGearRatio = 10.71;
+
+    private final LinearSystem<N2, N2, N2> mDrivetrainSystem = LinearSystemId.identifyDrivetrainSystem(1.98, 0.2, 1.5,
+      0.3);
+  private final DifferentialDrivetrainSim drivetrainSim = new DifferentialDrivetrainSim(
+      mDrivetrainSystem, DCMotor.getCIM(2), kGearRatio, kTrackWidth, kWheelRadius, null);
+
+ // private final DifferentialDriveOdometry odometry;
+
+  public static DriveTrain getInstance() {
+    if(instance == null) {
+      instance = new DriveTrain();
+    } 
+    return instance;
+  }
+
+
+  /** Creates a new DriveTrain. */
+  public DriveTrain() {
+
+    gyro = new AHRS(NavXComType.kMXP_SPI);
+    gyro.reset();
+
+    leftFront = new SimulatableSparkMax(DriveConstants.kLeftFrontPort, MotorType.kBrushed);
+    leftRear = new SimulatableSparkMax(DriveConstants.kLeftRearPort, MotorType.kBrushed);
+    rightFront = new SimulatableSparkMax(DriveConstants.kRightFrontPort, MotorType.kBrushed);
+    rightRear = new SimulatableSparkMax(DriveConstants.kRightRearPort, MotorType.kBrushed);
+
+    leftFront.setCANTimeout(250);
+    leftRear.setCANTimeout(250);
+    rightFront.setCANTimeout(250);
+    rightRear.setCANTimeout(250);
+
+    drive = new DifferentialDrive(leftFront, rightFront);
+
+    //leftEncoder = leftFront.getEncoder();
+    //rightEncoder = rightFront.getEncoder();
+
+    SparkMaxConfig globalConfig = new SparkMaxConfig();
+    SparkMaxConfig LFconfig = new SparkMaxConfig();
+    SparkMaxConfig LRconfig = new SparkMaxConfig();
+    SparkMaxConfig RFconfig = new SparkMaxConfig();
+    SparkMaxConfig RRconfig = new SparkMaxConfig();
+
+    globalConfig.smartCurrentLimit(DriveConstants.smartCurrentLimit).voltageCompensation(DriveConstants.voltageCompensation);
+    LFconfig.apply(globalConfig);
+    LRconfig.apply(globalConfig).follow(DriveConstants.kLeftFrontPort);
+    RFconfig.apply(globalConfig).inverted(true);//.encoder.inverted(true);
+    RRconfig.apply(globalConfig).follow(DriveConstants.kRightFrontPort).inverted(true);
+
+    leftFront.configure(LFconfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    leftRear.configure(LRconfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    rightFront.configure(RFconfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    rightRear.configure(RRconfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+    //leftEncoder.setPosition(0);
+    //rightEncoder.setPosition(0);
+
+    //odometry = new DifferentialDriveOdometry(gyro.getRotation2d(), leftEncoder.getPosition(), rightEncoder.getPosition());
+
+    SmartDashboard.putData("Field", field);
+    Logger.recordMetadata("Subsystem", "DriveTrain olusturuldu.");
+  }
+
+  public void drive(double speed, double rotation) {
+    drive.arcadeDrive(speed, rotation);
+    Logger.recordOutput("DriveTrain", "Robot hizi: " + speed);
+    Logger.recordOutput("DriveTrain", "Robot donus hizi: " + rotation);
+
+  }
+
+  
+  public void updatePosition(double forwardSpeed, double rotationSpeed) {
+    x += forwardSpeed * Math.cos(heading.getRadians()) * 0.02; 
+    y += forwardSpeed * Math.sin(heading.getRadians()) * 0.02; 
+
+    heading = heading.plus(Rotation2d.fromDegrees(rotationSpeed * 5));
+    field.setRobotPose(new Pose2d(new Translation2d(x, y), heading));
+  }
+
+ /*  
+  public Pose2d getPose() {
+    return odometry.getPoseMeters();
+  }
+
+  public void resetOdometry(Pose2d pose) {
+    odometry.resetPosition(gyro.getRotation2d(), leftEncoder.getPosition(), rightEncoder.getPosition(), pose);
+  }
+
+  public void updateOdometry() {
+    odometry.update(gyro.getRotation2d(), leftEncoder.getPosition(), rightEncoder.getPosition());
+  }
+*/
+  public void setGyroAngle(double angle) {
+    gyro.setAngleAdjustment(angle);
+    Logger.recordOutput("DriveTrain", "Gyro acisi ayarlandi: " + angle);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    //updateOdometry();
+    //field.setRobotPose(getPose());
+  }
+
+  @Override
+  public void simulationPeriodic() {
+      drivetrainSim.setInputs(
+        leftFront.get() * RobotController.getInputVoltage(),
+        rightFront.get() * RobotController.getInputVoltage()
+      );
+      drivetrainSim.update(0.02);
+  }
+}
